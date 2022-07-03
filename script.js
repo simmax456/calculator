4b9841b00943e07c229634721ca870cf126d895c
Simon Oladeji <simmax456@gmail.com>
2022-07-03 12:11:32 +0100

    calc: handle value either as number or symbol

--- script.js
+++ script.js
@@ -29,4 +29,63 @@ function buttonClick(value){
     renderOutput()
 };
 
+// if nothing has been clicked,
+//    change the 0 to what is clicked
 
+// if there is a value in the buffer and the user 
+//    wants to add to it, increment the buffer with value
+
+function handleAsNumber(value){
+    if (buffer == '0') {
+        buffer = value
+    }
+    else {
+        buffer += value
+    }
+};
+
+
+function handleAsOperator(value){
+    switch (value) {
+        // reset screen value on click
+        case 'AC':
+            runningTotal = 0;
+            buffer = '0';
+            lastOperator = null;
+            break;
+
+        // 
+        case '←':
+            if (buffer.length === 1) {
+                buffer = '0'
+            }
+            else {
+                buffer = buffer.substring(0, buffer.length - 1)
+            }
+            break;
+
+        case '=':
+            break;
+        
+        // case '.':
+        //     if (buffer === 0) {
+        //         buffer = parseFloat(0.)
+        //     }
+        //     break;
+        
+        case '%':
+        case '+':
+        case '-':
+        case '𝗑':
+        case '÷':
+            handleMath(value);
+            break;
+    };
+};
+
+
+// Show the output in our screen
+
+function renderOutput(){
+    screen.innerText = buffer
+};
